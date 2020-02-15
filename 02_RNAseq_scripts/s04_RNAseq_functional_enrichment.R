064c56c93e0d22e72e2351eeea6086425ea1441e
Lakhansing Pardeshi <lakhanp1@gmail.com>
2020-02-15 16:28:42 +0800

    updated RNAseq functional enrichment script

--- 02_RNAseq_scripts/s04_RNAseq_functional_enrichment.R
+++ 02_RNAseq_scripts/s04_RNAseq_functional_enrichment.R
@@ -8,7 +8,7 @@ library(grid)
 require(XLConnect)
 options(java.parameters = "- Xmx4g")
 xlcFreeMemory()
-library(org.AFumigatus293.eg.db)
+library(org.HSapiens.gencodev30.eg.db)
 
 
 ## RNAseq DEG functional analysis using clusterProfiler
@@ -20,25 +20,27 @@ source("E:/Chris_UM/GitHub/omics_util/02_RNAseq_scripts/s02_DESeq2_functions.R")
 
 ###########################################################################
 
-degResult <- "5A9_AA_vs_5A9_C"
+degResult <- "PLC5_CRI_CTCF_KO_vs_PLC5_CRI_Ctrl"
 
 file_RNAseq_info <- here::here("data", "RNAseq_info.txt")
-diffDataPath <- here::here("analysis", "01_RNAseq_data")
+diffDataPath <- here::here("analysis", "09_RNAseq_CTCF", "02_DESeq2_diff")
 
-outDir <- here::here("analysis", "01_RNAseq_data", degResult)
+outDir <- here::here("analysis", "09_RNAseq_CTCF", "02_DESeq2_diff", degResult)
 outPrefix <- paste(outDir, "/", degResult, sep = "")
 
-# file_msigDesc <- "E:/Chris_UM/Database/Human/GRCh38p12.gencode30/annotation_resources/msigDB_geneset_desc.tab"
+file_msigDesc <- "E:/Chris_UM/Database/Human/GRCh38p12.gencode30/annotation_resources/msigDB_geneset_desc.tab"
 
-orgDb <- org.AFumigatus293.eg.db
-keggOrg <- 'afm'
-keggIdCol <- "KEGG_ID"
-file_topGO <- "E:/Chris_UM/Database/A_fumigatus_293_version_s03-m05-r06/annotation_resources/geneid2go.AFumigatus_Af293.topGO.map"
+orgDb <- org.HSapiens.gencodev30.eg.db
+keggOrg <- 'hsa'
+keggIdCol <- "NCBI_ID"
+file_topGO <- "E:/Chris_UM/Database/Human/GRCh38p12.gencode30/annotation_resources/geneid2go.HSapiens.GRCh38p12.topGO.map"
 
 cutoff_qval <- 0.05
-cutoff_lfc <- 0.585
+cutoff_lfc <- 1
 cutoff_up <- cutoff_lfc
 cutoff_down <- -1 * cutoff_lfc
+
+col_lfc <- "log2FoldChange"
 
 ###########################################################################
 
@@ -53,26 +55,37 @@ degs <- suppressMessages(readr::read_tsv(file = rnaseqInfo$deg[1])) %>%
 if(! keggIdCol %in% colnames(degs)){
   keggInfo <- suppressMessages(
     AnnotationDbi::select(x = orgDb, keys = degs$geneId,
-                                    keytype = "GID", columns = keggIdCol)
+                          keytype = "ENSEMBL_VERSION", columns = keggIdCol)
   ) %>% 
     dplyr::filter(!is.na(!!sym(keggIdCol))) %>% 
-    dplyr::rename(geneId = GID)
+    dplyr::rename(geneId = ENSEMBL_VERSION)
   
   degs <- dplyr::left_join(x = degs, y = keggInfo, by = "geneId")
 }
-  
-downDegs <- dplyr::filter(degs, padj <= cutoff_qval & shrinkLog2FC <= cutoff_down)
-upDegs <- dplyr::filter(degs, padj <= cutoff_qval & shrinkLog2FC >= cutoff_up)
+
+downDegs <- dplyr::filter(degs, padj <= cutoff_qval & !!sym(col_lfc) <= cutoff_down) %>% 
+  dplyr::mutate(category = "down")
+
+upDegs <- dplyr::filter(degs, padj <= cutoff_qval & !!sym(col_lfc) >= cutoff_up) %>% 
+  dplyr::mutate(category = "up")
+
+degData <- dplyr::bind_rows(upDegs, downDegs)
 
 contrast <- unique(upDegs$contrast)
 
-geneList <- dplyr::filter(degs, !is.na(geneId)) %>% 
-  dplyr::select(geneId, rankMetric) %>% 
+geneList <- dplyr::filter(degs, !is.na(NCBI_ID)) %>% 
+  dplyr::select(NCBI_ID, rankMetric) %>% 
   tibble::deframe()
 
-###########################################################################
+## replace +Inf and -Inf values with max and min
+geneList[is.infinite(geneList) & geneList > 0] <- max(geneList[is.finite(geneList)]) + 1
+
+geneList[is.infinite(geneList) & geneList < 0] <- min(geneList[is.finite(geneList)]) - 1
+
+
+# ###########################################################################
 # ## clusterProfiler: GO enrichment
-# ego_up <- enrichGO(gene = unique(upDegs$geneId),
+# ego_up <- enrichGO(gene = unique(upDegs$ENSEMBL),
 #                    OrgDb = orgDb,
 #                    ont = "BP", pAdjustMethod = "BH",
 #                    pvalueCutoff = 0.05,
@@ -80,9 +93,13 @@ geneList <- dplyr::filter(degs, !is.na(geneId)) %>%
 #                    keyType = "ENSEMBL",
 #                    readable = FALSE)
 # 
+# barplot(ego_up, showCategory=20)
+# emapplot(ego_up, pie_scale=1.5,layout="kk", )
+# cnetplot(ego_up, showCategory = 10, node_label="category")
+# 
 # ego_up <- simplify(x = ego_up)
 # 
-# ego_down <- enrichGO(gene = unique(downDegs$geneId),
+# ego_down <- enrichGO(gene = unique(downDegs$ENSEMBL),
 #                      OrgDb = orgDb,
 #                      ont = "BP", pAdjustMethod = "BH",
 #                      pvalueCutoff = 0.05,
@@ -100,16 +117,31 @@ geneList <- dplyr::filter(degs, !is.na(geneId)) %>%
 # 
 # 
 # readr::write_tsv(x = ego_res, path = paste(outPrefix, ".clusterProfiler.GO.tab", sep = ""))
+# 
+# 
+# ego_degs <- compareCluster(geneClusters = ENSEMBL ~ category,
+#                fun = "enrichGO", data = degData,
+#                OrgDb = orgDb,
+#                ont = "BP", pAdjustMethod = "BH",
+#                pvalueCutoff = 0.05,
+#                qvalueCutoff = 0.05,
+#                keyType = "ENSEMBL",
+#                readable = FALSE)
+# 
+# dotplot(ego_degs,
+#         showCategory = 20)
+# emapplot(ego_degs)
+
 
 
 ## topGO GO enrichment
 topgo_up <- topGO_enrichment(goMapFile = file_topGO,
-                             genes = unique(upDegs$geneId),
+                             genes = unique(upDegs$ENSEMBL),
                              type = "BP",
                              goNodeSize = 5)
 
 topgo_down <- topGO_enrichment(goMapFile = file_topGO,
-                               genes = unique(downDegs$geneId),
+                               genes = unique(downDegs$ENSEMBL),
                                type = "BP",
                                goNodeSize = 5)
 
@@ -120,6 +152,22 @@ topgo_res <- dplyr::bind_rows(
   dplyr::mutate(contrast = contrast)
 
 readr::write_tsv(x = topgo_res, path = paste(outPrefix, ".topGO.tab", sep = ""))
+
+## top 10 GO term bar plot
+topgoPlotDf <- dplyr::group_by(topgo_res, category) %>% 
+  dplyr::arrange(weightedFisher, .by_group = TRUE) %>% 
+  dplyr::slice(1:10) %>% 
+  dplyr::ungroup()
+
+
+topgo_bar <- enrichment_bar(df = topgoPlotDf,
+                            title = paste(degResult, "\ntop 10 enriched GO terms in up and down DEGs"))
+
+png(filename = paste(outPrefix, ".topGO_bar.png", sep = ""),
+    width = 2500, height = 2500, res = 250)
+
+topgo_bar
+dev.off()
 
 ###########################################################################
 # ## clusterProfiler: KEGG pathway enrichment
@@ -183,45 +231,46 @@ keggp_res <- dplyr::bind_rows(
 
 readr::write_tsv(x = keggp_res, path = paste(outPrefix, ".keggProfile.tab", sep = ""))
 
-# ###########################################################################
-# ## GSEA
-# msigdbr_show_species()
-# msig_df <- msigdbr(species = "Danio rerio", category = c("H", "C2", "C5")) %>% 
-#   dplyr::filter(! gs_subcat %in% c("MF", "CC"))
-# 
-# msig_list <- split(x = msig_df$entrez_gene, f = msig_df$gs_name)
-# 
-# length(intersect(names(geneList), unique(msig_df$entrez_gene)))
-# 
-# vn <- VennDiagram::venn.diagram(
-#   x = list(geneList = names(geneList), msig = unique(msig_df$entrez_gene)),
-#   filename = NULL,
-#   print.mode = c("raw", "percent"),
-#   scaled = FALSE
-# )
-# dev.off()
-# grid.draw(vn)
-# 
-# # msigDescDf <- suppressMessages(readr::read_tsv(file = file_msigDesc))
-# # msigDesc <- split(x = msigDescDf$DESCRIPTION_BRIEF, f = msigDescDf$STANDARD_NAME)
-# 
-# egsea <- GSEA(geneList = geneList,
-#               nPerm = 10000,
-#               pvalueCutoff = 0.1,
-#               minGSSize = 10, maxGSSize = Inf,
-#               TERM2GENE = dplyr::select(msig_df, gs_name, entrez_gene))
-# 
-# egseaDf <- as_tibble(egsea) %>% 
-#   dplyr::left_join(y = msigDescDf, by = c("ID" = "STANDARD_NAME")) %>% 
-#   dplyr::mutate(contrast = contrast) %>% 
-#   dplyr::select(ID, contrast, everything(), -Description)
-# 
-# readr::write_tsv(x = egseaDf,
-#                  path = paste(outPrefix, ".clusterProfiler.GSEA.tab", sep = ""))
-# 
-# 
-# 
-# 
+###########################################################################
+## GSEA
+msigdbr_show_species()
+msig_df <- msigdbr(species = "Homo sapiens") %>% 
+  dplyr::filter(gs_cat %in% c("H", "C2", "C5")) %>% 
+  dplyr::filter(! gs_subcat %in% c("MF", "CC"))
+
+# , category = c("H", "C2", "C5")
+msig_list <- split(x = msig_df$entrez_gene, f = msig_df$gs_name)
+
+length(intersect(names(geneList), unique(msig_df$entrez_gene)))
+
+vn <- VennDiagram::venn.diagram(
+  x = list(geneList = names(geneList), msig = unique(msig_df$entrez_gene)),
+  filename = NULL,
+  print.mode = c("raw", "percent"),
+  scaled = FALSE
+)
+dev.off()
+grid.draw(vn)
+
+msigDescDf <- suppressMessages(readr::read_tsv(file = file_msigDesc))
+msigDesc <- split(x = msigDescDf$DESCRIPTION_BRIEF, f = msigDescDf$STANDARD_NAME)
+
+egsea <- GSEA(geneList = geneList,
+              nPerm = 10000,
+              pvalueCutoff = 0.1,
+              minGSSize = 10, maxGSSize = Inf,
+              TERM2GENE = dplyr::select(msig_df, gs_name, entrez_gene))
+
+egseaDf <- as_tibble(egsea) %>%
+  dplyr::left_join(y = msigDescDf, by = c("ID" = "STANDARD_NAME")) %>%
+  dplyr::mutate(contrast = contrast) %>%
+  dplyr::select(ID, contrast, everything(), -Description)
+
+readr::write_tsv(x = egseaDf,
+                 path = paste(outPrefix, ".clusterProfiler.GSEA.tab", sep = ""))
+
+
+
 # ## plotting specific genesets
 # genesetSub <- c("GO_CELL_CYCLE",
 #                 "GO_RESPONSE_TO_ENDOPLASMIC_RETICULUM_STRESS",
@@ -233,17 +282,17 @@ readr::write_tsv(x = keggp_res, path = paste(outPrefix, ".keggProfile.tab", sep 
 #     width = 10, height = 8, onefile = TRUE)
 # 
 # for (setId in genesetSub) {
-#   
+# 
 #   if(setId %in% egsea$ID){
 #     pt <- enrichplot::gseaplot2(egsea, geneSetID = setId)
 #     wrap_100 <- wrap_format(120)
-#     
+# 
 #     plotSubTitle <- paste(
 #       "p-value = ", sprintf(fmt = "%.2E", egseaDf$pvalue[which(egseaDf$ID == setId)]),
 #       "; q-value = ", sprintf(fmt = "%.2E", egseaDf$qvalues[which(egseaDf$ID == setId)]),
 #       "\n", wrap_100(x = msigDesc[[setId]]),
 #       sep = "")
-#     
+# 
 #     pt <- pt +
 #       labs(
 #         title = paste(setId, ": ", contrast, sep = ""),
@@ -252,21 +301,29 @@ readr::write_tsv(x = keggp_res, path = paste(outPrefix, ".keggProfile.tab", sep 
 #       theme_bw() +
 #       theme(panel.grid = element_blank(),
 #             panel.border = element_blank())
-#     
+# 
 #     plotList[[setId]] <- pt
-#     
+# 
 #     plot(pt)
-#     
+# 
 #   }
-#   
+# 
 # }
 # 
 # dev.off()
-# 
-# 
+
+
 # ###########################################################################
 # ## GSEA enrichment using fgsea
 # gseaRes <- fgsea(pathways = msig_list, stats = geneList, nperm = 10000)
+# 
+# gseaRes <- dplyr::filter(gseaRes, pval < 0.05) %>%
+#   dplyr::left_join(y = msigDescDf, by = c("pathway" = "STANDARD_NAME")) %>%
+#   dplyr::mutate(contrast = contrast)
+# 
+# topPathways <- gseaRes[head(order(pval), n=15)][order(NES), pathway]
+# plotGseaTable(msig_list[topPathways], geneList,
+#               gseaRes, gseaParam=0.5)
 # 
 # pt2 <- plotEnrichment(pathway = msig_list[[setId]],
 #                       stats = geneList) +
@@ -279,8 +336,8 @@ readr::write_tsv(x = keggp_res, path = paste(outPrefix, ".keggProfile.tab", sep 
 #   theme(panel.grid = element_blank(),
 #         axis.text = element_text(size = 12),
 #         axis.title = element_text(size = 14, face = "bold"))
-# 
-# 
+
+
 
 
 # ###########################################################################
@@ -303,6 +360,17 @@ writeWorksheet(object = exc, data = keggp_res, sheet = wrkSheet)
 setAutoFilter(object = exc, sheet = wrkSheet,
               reference = aref(topLeft = "A1", dimension = dim(keggp_res)))
 
+setColumnWidth(object = exc, sheet = 1:2, column = 1, width = -1)
+setColumnWidth(object = exc, sheet = 1:2, column = 2, width = c(13000))
+
+wrkSheet <- "GSEA"
+createSheet(exc, name = wrkSheet)
+createFreezePane(exc, sheet = wrkSheet, 2, 2)
+writeWorksheet(object = exc, data = egseaDf, sheet = wrkSheet)
+setAutoFilter(object = exc, sheet = wrkSheet,
+              reference = aref(topLeft = "A1", dimension = dim(egseaDf)))
+setColumnWidth(object = exc, sheet = 3, column = 1, width = c(13000))
+
 # wrkSheet <- "clusterProfiler_GO"
 # createSheet(exc, name = wrkSheet)
 # createFreezePane(exc, sheet = wrkSheet, 2, 2)
@@ -317,8 +385,6 @@ setAutoFilter(object = exc, sheet = wrkSheet,
 # setAutoFilter(object = exc, sheet = wrkSheet,
 #               reference = aref(topLeft = "A1", dimension = dim(ekegg_res)))
 
-setColumnWidth(object = exc, sheet = 1:2, column = 1, width = -1)
-setColumnWidth(object = exc, sheet = 1:2, column = 2, width = c(13000))
 
 xlcFreeMemory()
 saveWorkbook(exc)
